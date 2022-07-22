3e9f9776b98a72d59409cccf2029cd4f29d6d98a
Charlan-Barneche <charlanbarneche02@gmail.com>
2022-07-22 11:47:42 -0300

    site mobile v0.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,32 @@
-let seta = document.getElementById ("seta");
-let entry = document.getElementById ("entry");
-seta.addEventListener ("click", click);
 
-let initialScreen = document.getElementById ("initial-screen");
 
-function click (click) {
+let seta = document.getElementById("seta");
+let entry = document.getElementById("entry");
+seta.addEventListener("click", click);
+
+let initialScreen = document.getElementById("initial-screen");
+
+function click(click) {
     initialScreen.classList = ('initial-screen scroll-scren hidden-screen');
     entry.classList = ('btn btn-cor btn-move')
 }
 
+
+const ITEM = document.querySelectorAll("[data-anime]");
+
+const ANIME_SCROLL = () => {
+    const WINDOW_TOP = window.pageYOffset * window.innerHeight * 0.10;
+    console.log (WINDOW_TOP)
+
+    ITEM.forEach(element => {
+        if (WINDOW_TOP > element.offsetTop) {
+            element.classList.add('scroll-scren' ,'hidden-screen');
+        }
+    })
+}
+
+
+window.addEventListener("scroll", () => {
+    ANIME_SCROLL();
+})
 
