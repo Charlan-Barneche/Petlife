368a09182774e7d92624ce67a1cfd911bf491474
Charlan-Barneche <charlanbarneche02@gmail.com>
2022-07-23 22:23:35 -0300

    tela de login adicionada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,48 @@
 // animation click initial screen
 
-const SETA = document.getElementById("seta");
-const ENTRY = document.getElementById("entry");
-SETA.addEventListener("click", click);
+    const SETA = document.getElementById("seta");
+    const ENTRY = document.getElementById("entry");
+    const REGISTER_ENTRY = document.getElementById ("register-entry");
+    SETA.addEventListener("click", click);
 
-let initialScreen = document.getElementById("initial-screen");
+    let initialScreen = document.getElementById("initial-screen");
 
-function click(click) {
-    initialScreen.classList = ('initial-screen scroll-scren hidden-screen');
-    ENTRY.classList = ('btn btn-cor btn-move')
-}
+    function click(click) {
+        initialScreen.classList = ('initial-screen scroll-scren hidden');
+        REGISTER_ENTRY.classList.add("up");
 
-// fim animation click initial screen
+        ENTRY.classList = ('btn btn-move-cor')
 
-// animation scroll
+    }
 
-const ITEM = document.querySelectorAll("[data-anime]");
+    // fim animation click initial screen
 
-const ANIME_SCROLL = () => {
-    const WINDOW_TOP = window.pageYOffset * window.innerHeight * 0.10;
-    //console.log (WINDOW_TOP)
+    // animation scroll
 
-    ITEM.forEach(element => {
-        if (WINDOW_TOP > element.offsetTop) {
-            element.classList.add('scroll-scren' ,'hidden-screen');
-        }
+    const ITEM = document.querySelectorAll("[data-anime]");
+
+    const ANIME_SCROLL = () => {
+        const WINDOW_TOP = window.pageYOffset * window.innerHeight * 0.10;
+        // console.log (WINDOW_TOP)
+
+        ITEM.forEach(element => {
+            if (WINDOW_TOP > element.offsetTop) {
+                element.classList.add('scroll-scren' ,'hidden');
+                REGISTER_ENTRY.classList.add("up");
+
+
+                
+            }
+        })
+    }
+
+
+    window.addEventListener("scroll", () => {
+        ANIME_SCROLL();
     })
-}
-
-
-window.addEventListener("scroll", () => {
-    ANIME_SCROLL();
-})
 
 // fim animation scroll
+
+// click btn entrar e tela de login
+
+
